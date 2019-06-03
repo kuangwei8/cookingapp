8cc3142c816f74b13ce06aa6ce48c39861d209bb
Jessica Munoz and Wei Kuang <wei@>
2019-06-03 14:42:22 -0700

    It will have various conversion formulas

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+/*var gallonsToLiters = function(){
+  var gallon = parseFloat(prompt("Enter number of gallons:"));
+  var liters = (gallon*3.7854);
+  result = alert(liters);
+};
+gallonsToLiters();
+var teaspoonToTablespoon = function(){
+  var teaspoon = parseFloat(prompt("Enter number of teaspoons: "));
+  var tablespoon = (teaspoon / 3);
+  result = alert("Tablespoons:" + tablespoon);
+};
+teaspoonToTablespoon();
+
+
+var quartsToGallon = function(){
+  var quart = parseFloat(prompt("Enter number of quarts:"));
+  var gallon = (quart/4);
+  result = alert ("gallon: " + gallon);
+};
+quartsToGallon();
+
+var ozToPd = function() {
+  var oz = parseFloat(prompt("Enter number of ounces"));
+  var pd = (oz / 16);
+  result = alert("Number of pounds: " + pd);
+};
+ozToPd();
+*/
+
+var tblsToCups = function (){
+  var tablespoon = parseFloat(prompt("Enter number of Tablespoons"));
+  var cup = (tablespoon/16);
+  result = alert("Number of cups: " + cup);
+} ;
+tblsToCups();
